493d15bb08c7b91b212b2cc45ebd2cae9e00273c
Chad-Anthony <Chad-Anthony@10.100.248.234>
2017-09-21 09:54:07 -0400

    First commit

---
+++ Integration Project-CAS/src/CAS.java
@@ -0,0 +1,26 @@
+// A class is code
+public class CAS {
+/* Chad-Anthony Schloss 
+ * 
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("What's Good Y'all Boys");
+// stuff in quotes gets output exactly
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println("Once Upon a Time");
+		
+		Scanner scan = new Scanner(System.in); 
+		int r = 12;
+		scan.nextLine();
+		double c = 4.0;
+		String a = ("is the best place to learn and practice coding!");
+		
+		System.out.println(r);
+		System.out.println(c + a);
+	
+	}
+
+}
